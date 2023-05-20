1fde66d510bf2fa81209be1224943c5b24643cef
5hubham5ingh <shubhams@mytaxcafe.com>
2023-05-20 18:36:55 +0530

    fixed login modal and background scrolling

--- src/Components/Background.jsx
+++ src/Components/Background.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import background from "../images/background.png";
+
 function Background() {
   return (
     <div
       style={{
         backgroundImage: `url(${background})`,
+        backgroundRepeat: "round",
         height: "220vh",
         width: "100vw",
         textAlign: "center",
         verticalAlign: "center",
-        backgroundSize: "100%",
+        // backgroundSize: "100%",
+        backgroundSize: "cover",
         // filter: "blur(5px)",
       }}
     ></div>
--- src/Components/LogIn.jsx
+++ src/Components/LogIn.jsx
@@ -1,14 +1,45 @@
 import { Box, Button, Grid, Modal, TextField, Typography } from "@mui/material";
-import { forwardRef, useImperativeHandle, useState } from "react";
+import { forwardRef, useEffect, useImperativeHandle, useState } from "react";
 
 const LogIn = forwardRef((props, ref) => {
   const [open, setOpen] = useState(false);
-  const handleClose = () => setOpen(false);
-  const handleOpen = () => setOpen(true);
-  console.log(open);
+  const handleClose = () => {
+    setOpen(false);
+    enableScroll();
+  };
+  const handleOpen = () => {
+    disableScroll();
+    setOpen(true);
+  };
+
   useImperativeHandle(ref, () => {
     return { open: handleOpen };
   });
+
+  let scrollX;
+  let scrollY;
+
+  function enableScroll() {
+    // Unfix the body element
+    document.body.style.position = "";
+    document.body.style.top = "";
+    document.body.style.left = "";
+
+    // Restore the scroll position
+    window.scrollTo(scrollX, scrollY);
+  }
+
+  function disableScroll() {
+    // Store the current scroll position
+    scrollX = window.scrollX;
+    scrollY = window.scrollY;
+
+    // Fix the body element
+    document.body.style.position = "fixed";
+    document.body.style.top = `-${scrollY}px`;
+    document.body.style.left = `-${scrollX}px`;
+  }
+
   const style = {
     position: "absolute",
     top: "40%",
@@ -29,7 +60,7 @@ const LogIn = forwardRef((props, ref) => {
       aria-describedby="log-in-page"
     >
       <Box sx={style}>
-        <Grid container spacing={2}>
+        <Grid container component={"form"} spacing={2}>
           <Grid item xs={12} sm={6}>
             <TextField label="Account name" fullWidth />
           </Grid>
@@ -39,7 +70,13 @@ const LogIn = forwardRef((props, ref) => {
           <Grid item xs={12}>
             <Button
               variant="contained"
-              sx={{ backgroundColor: "lightsteelblue" }}
+              sx={{
+                backgroundColor: "lightsteelblue",
+                float: "right",
+                borderRadius: "10px",
+                color: "black",
+                "&:hover": { backgroundColor: "darkblue", color: "white" },
+              }}
               type="submit"
             >
               Submit
