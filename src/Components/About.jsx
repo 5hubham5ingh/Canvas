3b728361920f1348e16ff459a384697cd5aea885
5hubham5ingh <shubhams@mytaxcafe.com>
2023-06-16 17:42:43 +0530

    adding creator's socials in about section

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,25 +1,36 @@
-import { Box, Typography } from "@mui/material";
+import { GitHub, Twitter } from "@mui/icons-material";
+import { Box, IconButton, Stack, Typography } from "@mui/material";
 import React from "react";
 
 export default function About() {
+  const openTwitter = () => alert("dasas");
+  const openGitHub = () => window.open();
   return (
     <Box
       sx={{
         position: "relative",
         color: "aqua",
         backgroundImage: "linear-gradient(transparent, black, black)",
-        top: "210vh",
+        top: "200vh",
         width: "100%",
       }}
     >
       <Typography paragraph p={2} id={"about"}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et cumque
-        dicta, ea asperiores, eligendi nobis error ex magnam atque, ab
-        voluptatibus consequuntur dolores minus repellat! Architecto magnam
-        quaerat dignissimos illo. Lorem, ipsum dolor sit amet consectetur
-        adipisicing elit. Sapiente aliquam rem voluptate. Eaque nesciunt officia
-        modi molestiae delectus, minus odio placeat nam necessitatibus, itaque
-        commodi et in nobis ducimus. Soluta?
+        This application is designed only for demonstrative and educational
+        purposes. It can be used to design no-code HTML, pdf and excel files,
+        and generate reports for data visualization, all from single file
+        source.
+        <Stack direction="row" p={2}>
+          Creator's socials :-
+          <IconButton variant="contained" color="white" onClick={openTwitter}>
+            <Twitter color="blue" />
+            <Typography color="white">@5hubhamSingh</Typography>
+          </IconButton>
+          <IconButton variant="contained" onClick={openGitHub}>
+            <GitHub />
+            <Typography>5hubham5ingh</Typography>
+          </IconButton>
+        </Stack>
       </Typography>
     </Box>
   );
