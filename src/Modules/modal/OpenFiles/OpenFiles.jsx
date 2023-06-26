ae4cd92e44bc936a1f263ee1a2a2ead7ad0257d3
5hubham5ingh <shubhams@mytaxcafe.com>
2023-06-26 18:35:38 +0530

    added new MUI component in openfiles component

--- src/Modules/modal/OpenFiles/OpenFiles.jsx
+++ src/Modules/modal/OpenFiles/OpenFiles.jsx
@@ -1,18 +1,36 @@
-import { Stack } from "@mui/material";
+import { List, ListItem, ListItemText, Stack } from "@mui/material";
 import { useUser } from "../../User/userContext";
 
-export default function OpenFiles(){
-    debugger
-    const {user} = useUser();
-const files = user.files;
-const handleClick=(e)=>{
-    console.log(e.target.name)
-}
-    return (<>
-    <Stack direction="row">
-{
-    files.map((file,index)=> <span key={index} name={file.id} onClick={handleClick}>{file.displayName}</span>)
-}
-    </Stack>
-    </>)
+export default function OpenFiles() {
+  debugger;
+  const { user } = useUser();
+  const files = user.files;
+  const handleClick = (e) => {
+    console.log(e);
+  };
+  return (
+    <>
+      <Stack direction="column">
+        {/* {files.map((file, index) => (
+          <span key={file.id} name={file.id} onClick={handleClick}>
+            {"📜" + file.displayName}
+          </span>
+        ))} */}
+        <List>
+          {files.map((file) => (
+            <ListItem
+              button
+              component="a"
+              href={file.id}
+              key={file.id}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <ListItemText primary={file.displayName} />
+            </ListItem>
+          ))}
+        </List>
+      </Stack>
+    </>
+  );
 }
